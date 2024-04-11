bb808b33df9fd2fda42124d7669138b38eca8d99
nachorsc <nacho.rodriguez162@gmail.com>
2024-04-11 17:40:37 -0300

    subiendo repositorio Mercado Liebre

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+const publicPath = path.resolve(__dirname, "../public");
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Servidor ejecutandose en http://localhost:' + port);
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.post('/login', (req, res) => {
+
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.post('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
